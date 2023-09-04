7b5871ce00205194a28d4c30a6cec517c91351b7
nojinee <dsg03-2@daun.met>
2023-09-04 17:57:18 +0900

    [LITTLE_FIX] : 🔧 불필요한 파일 삭제

--- app/src/main/java/com/example/phone_list/MainContactActivity.kt
+++ app/src/main/java/com/example/phone_list/MainContactActivity.kt
@@ -2,10 +2,13 @@ package com.example.phone_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.phone_list.databinding.ActivityMainContactBinding
 
 class MainContactActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainContactBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_contact)
+        setContentView(binding.root)
     }
 }
