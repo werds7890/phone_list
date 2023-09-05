a52a91c95db854ce47a721d28896a49c1fef09af
hwajiny <ghkwls02224@gmail.com>
2023-09-05 14:36:47 +0900

    Main data move to ContactListFragment

--- app/src/main/java/com/example/phone_list/MainContactActivity.kt
+++ app/src/main/java/com/example/phone_list/MainContactActivity.kt
@@ -1,14 +1,11 @@
 package com.example.phone_list
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.phone_list.databinding.ActivityMainContactBinding
 import com.google.android.material.tabs.TabLayoutMediator
-import java.util.jar.Attributes.Name
 
 class MainContactActivity : AppCompatActivity() {
-    var datalist = ArrayList<Maindata>{}
     private val binding by lazy { ActivityMainContactBinding.inflate(layoutInflater) }
 
     private val tabTitleArray = arrayOf(
@@ -20,7 +17,6 @@ class MainContactActivity : AppCompatActivity() {
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = MainContactActivity.inflate(layoutInflater)
         setContentView(binding.root)
 
         var viewPager = binding.viewpager
@@ -33,29 +29,6 @@ class MainContactActivity : AppCompatActivity() {
             tab.icon = getDrawable(tabIconArray[position])
         }.attach()
 
-        val dataList = mutableListOf<Maindata>()
-        dataList.add(Maindata(profileImage = "sn", aname = "써니"))
-        dataList.add(Maindata(profileImage = "hy", aname = "효연"))
-        dataList.add(Maindata(profileImage = "ty", aname = "태연"))
-        dataList.add(Maindata(profileImage = "yr", aname = "유리"))
-        dataList.add(Maindata(profileImage = "js", aname = "제시카"))
-        dataList.add(Maindata(profileImage = "tp", aname = "티파니"))
-        dataList.add(Maindata(profileImage = "sh", aname = "서현"))
-        dataList.add(Maindata(profileImage = "ya", aname = "윤아"))
-        dataList.add(Maindata(profileImage = "sy", aname = "수영"))
-
-
-
-
-       
-        binding.listView.adapter = MainAdapter(this, dataList)
-
-       
-        binding.listView.setOnItemClickListener{ parent, view, position, id ->
-            val name: String = (binding.listView.adapter.getItem(position) as MyItem).aName
-            Toast.makeText(this," $name 선택!", Toast.LENGTH_SHORT).show()
-
 
         }
     }
-}
--- app/src/main/java/com/example/phone_list/Maindata.kt
+++
@@ -1,5 +0,0 @@
-package com.example.phone_list
-
-data class Maindata(var aname: String, var profileImage: String) {
-
-}
